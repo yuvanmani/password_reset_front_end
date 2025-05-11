970d84c72607ee8e0b3f25559e988d9d09a9a7f3
Manivannan <yuvanmani007@gmail.com>
2025-05-12 03:07:14 +0530

    back end URL added in services/instance

--- src/services/instance.js
+++ src/services/instance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://127.0.0.1:3001/api/v1/users";
+const baseURL = "https://password-reset-back-end-cunz.onrender.com/api/v1/users";
 
 const instance = axios.create({
     baseURL,
